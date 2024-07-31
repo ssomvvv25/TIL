6ecbf953dfe61a60f05f05d93f126d501f0cde1a
소민 <103200925+ssomvvv25@users.noreply.github.com>
2024-07-31 16:57:21 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 44796 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,18 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
-a = set()
-b = set()
-result = []
-
-for _ in range(N):
-    a.add(input())
-for _ in range(M):
-    b.add(input())
-
-for i in a:
-    if i in b:
-        result.append(i)
-result.sort()
-print(len(result))
-for i in result:
-    print(i)
+listen = [input().rstrip() for _ in range(N)]
+see = [input().rstrip() for _ in range(M)]
+# 교집합 구하기
+ans = list(set(listen) & set(see))
+# 오름차순으로 정렬
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)
