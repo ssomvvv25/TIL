52222a8a05f4d420af9709044180066076e31170
소민 <103200925+ssomvvv25@users.noreply.github.com>
2025-01-20 06:07:43 +0900

    [Silver IV] Title: 스택 2, Time: 792 ms, Memory: 88788 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,31 +1,28 @@
-# 리스트로 간단하게 스택을 구현할 수 있다.
-
 import sys
 
 stack = []
 
 n = int(sys.stdin.readline())
 
-for _ in range(n):
+for _ in range(n): # 5가지 명령에 대한 코드 작성
     command = sys.stdin.readline().split()
-
     if command[0] == '1':
         stack.append(command[1])
     elif command[0] == '2':
-        if stack:
+        if stack: # 스택에 정수가 있다면
             print(stack.pop())
         else:
             print(-1)
     elif command[0] == '3':
         print(len(stack))
-    elif command[0] =='4':
+    elif command[0] == '4':
         if stack:
             print(0)
         else:
             print(1)
     elif command[0] == '5':
         if stack:
-            print(stack[-1])
+            print(stack[-1]) # 맨 위의 정수 출력
         else:
             print(-1)
 
