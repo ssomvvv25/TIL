035ecee1f505201a31bf681cce503a9f475eaf5d
소민 <103200925+ssomvvv25@users.noreply.github.com>
2025-01-23 06:02:10 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def backtracking() :
+def backtracking():
+    # 원하는 길이의 순열이 완성되면 출력
     if len(array) == m:
-        print(" ".join(map(str, array)))
+        print(" ".join(map(str,array)))
         return
-    for i in range(1, n+1):
-        if i not in array:
+    # i는 1부터 n까지의 숫자
+    for i in range(1,n+1):
+        if i not in used:
             array.append(i)
+            used.add(i) # set에 추가
             backtracking()
             array.pop()
+            used.remove(i) # set에서 제거
+
 
 n,m = map(int, input().split())
-array=[]
-backtracking()
+array=[] # 수열을 저장할 리스트
+used = set()
+backtracking() # 백트래킹 시작
