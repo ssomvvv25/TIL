9c80cbebbe48b8a0f9fb27ab4ba8f13cceea0504
소민 <103200925+ssomvvv25@users.noreply.github.com>
2025-01-23 14:39:06 +0900

    [Silver III] Title: N과 M (3), Time: 1124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,14 @@
-import sys
-input = sys.stdin.readline
+n, m = map(int, input().split())
 
-def backtracking() :
-    if len(arr) == m:
-        print(*arr)
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str,s)))
         return
+    
     for i in range(1,n+1):
-        arr.append(i)
-        backtracking()
-        arr.pop()
-
-n,m = map(int, input().split())
-arr=[]
-backtracking()
+        s.append(i)
+        dfs()
+        s.pop()
+dfs()
